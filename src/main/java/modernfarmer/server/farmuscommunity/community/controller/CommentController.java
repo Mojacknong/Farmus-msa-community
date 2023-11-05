654712f118d0e53d664f2016c1b07fc07b66290d
seungryeol <opop0421@naver.com>
2023-11-05 20:39:31 +0900

    [add] 커뮤니티 기본 세팅(유레카 등록, ci/cd)

--- src/main/java/modernfarmer/server/farmuscommunity/community/controller/CommentController.java
+++ src/main/java/modernfarmer/server/farmuscommunity/community/controller/CommentController.java
@@ -1,10 +1,16 @@
 package modernfarmer.server.farmuscommunity.community.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/community/comment")
 public class CommentController {
+
+    @GetMapping()
+    public String test(){
+        return "HI";
+    }
 }
